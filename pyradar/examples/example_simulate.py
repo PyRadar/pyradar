729291c0f6822a74c4a97ef4e41a1ed4073bb049
JuanBC <jbc.develop@gmail.com>
2013-07-05 19:21:02 -0300

    added headers

--- pyradar/examples/example_simulate.py
+++ pyradar/examples/example_simulate.py
@@ -1,4 +1,26 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+# Copyright 2012 - 2013
+# Matias Herranz <matiasherranz@gmail.com>
+# Joaquin Tita <joaquintita@gmail.com>
+#
+# hhttps://github.com/PyRadar/pyradar
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+
 from pyradar.simulate.image_simulator import ImageSimulator
 from pyradar.utils.timeutils import Timer
 pylab.close()
